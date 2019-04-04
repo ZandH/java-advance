07bd8831ee0db347b9b20fb321c8f61db1697fd9
review <1326368622@qq.com>
2019-04-04 09:17:43 +0800

    带缓冲的输入输出流代码2

---
+++ io-study/src/com/soft1841/io/BufferedTest.java
@@ -0,0 +1,43 @@
+package com.soft1841.io;
+
+import java.io.*;
+import java.nio.Buffer;
+
+public class BufferedTest {
+    public static void main(String[] args) throws IOException {
+        //定义字符串数组
+        String content[] = {"你不喜欢我，", "我一点都不介意。", "因为我活下来，", "不是为了取悦你！"};
+        //创建文本对象
+        File file = new File("word.txt");
+        try {
+            //创建FileWrite类对象
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bufw = new BufferedWriter(fw);
+            // 遍历循环数组
+            for (int k = 0; k < content.length; k++) {
+                bufw.write(content[k]);
+                bufw.newLine();
+            }
+            bufw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            FileReader fr = new FileReader(file);
+            //创建BufferedReader类对象
+            BufferedReader bufr = new BufferedReader(fr);
+            String s = null;
+            int i = 0;
+            //如果文件的文本行数不为null，则进入循环
+            while ((s = bufr.readLine()) != null) {
+                i++;
+                System.out.println("第" + i + "行" + s);
+            }
+            bufr.close();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
