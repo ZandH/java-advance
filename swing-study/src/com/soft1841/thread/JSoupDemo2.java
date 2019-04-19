56acffdc589932b36e7d794bcd713a31ef7de856
review <1326368622@qq.com>
2019-04-19 12:50:12 +0800

    爬取图片

---
+++ swing-study/src/com/soft1841/thread/JSoupDemo2.java
@@ -0,0 +1,52 @@
+package com.soft1841.thread;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.UUID;
+
+/**
+ * 爬取图片
+ */
+
+public class JSoupDemo2 {
+    public static void main(String[] args) throws Exception {
+        File file;
+        InputStream inputStream;
+
+        OutputStream outputStream;
+        String url = "https://www.douban.com/photos/album/1686028210/";
+        Connection connection = Jsoup.connect(url);
+        Document document = connection.get();
+        Elements elements = document.getElementsByClass("photo_wrap");
+        System.out.println(elements.size());
+        for (Element e : elements) {
+            Element imgElement = e.child(0).child(0);
+            UUID uuid = UUID.randomUUID();
+            String imgName = uuid + ".jpg";
+            file = new File("D:\\carousel\\" + imgName);
+            URL url1 = new URL(imgElement.attr("src"));
+            URLConnection uc = url1.openConnection();
+            inputStream = uc.getInputStream();
+            outputStream = new FileOutputStream(file);
+            int temp;
+            byte[] buf = new byte[1024];
+            while ((temp = inputStream.read(buf)) != -1) {
+                outputStream.write(buf, 0, temp);
+
+            }
+            outputStream.close();
+            inputStream.close();
+
+        }
+    }
+}
