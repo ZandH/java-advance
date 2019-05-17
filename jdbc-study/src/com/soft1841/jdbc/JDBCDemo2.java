de168dd283c9b7c42b9b9b8241ec3bb04e409d2f
review <1326368622@qq.com>
2019-05-17 12:27:31 +0800

    jdbc的学习

--- jdbc-study/src/com/soft1841/jdbc/JDBCDemo2.java
+++
@@ -1,4 +0,0 @@
-package com.soft1841.jdbc;
-
-public class JDBCDemo2 {
-}
---
+++ jdbc-study/src/main/java/com/soft1841/jdbc/JDBCDemo3.java
@@ -0,0 +1,35 @@
+package com.soft1841.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * JDBC更新数据
+ * @author 屏儿
+ * 2019/05/17
+ */
+public class JDBCDemo3 {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/db_jdbc?useUnicode = true&characterEncoding=utf8";
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName(JDBC_DRIVER);
+        System.out.println("连接数据库.....");
+        Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+        String sql = "UPDATE t_student SET name=?,age=? WHERE id=?";
+        System.out.println("实例化PreparedStatement对象");
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1,"修改后的姓名");
+        pstmt.setInt(2,12);
+        // 此处设置需要修改的那条记录的id，
+        pstmt.setInt(3,5);
+        int n = pstmt.executeUpdate();
+        System.out.println(n + "行记录受影响");
+        pstmt.close();
+        conn.close();
+    }
+}
