67fa6fb8c526473735d8374edf0ea738b68c0a10
review <1326368622@qq.com>
2019-05-21 09:25:21 +0800

    设计模式的学习

--- daily-study/src/com/soft1841/daily/FactoryTest.java
+++ daily-study/src/com/soft1841/daily/FactoryTest.java
@@ -1,5 +1,10 @@
 package com.soft1841.daily;
 
+/**
+ * 测试类
+ * @author 屏儿
+ * 2019/05/21
+ */
 public class FactoryTest {
     public static void main(String[] args) {
         Shape circle = ShapeFactory.getCircleInstance();
